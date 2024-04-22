ebd5afdfd898e5d1d1533c54ec4b407e00ab0878
pinableed <136363083+pinableed@users.noreply.github.com>
2024-04-22 15:23:00 +0200

    Commit 02 desde VSC

---
+++ 01_variables.py
@@ -0,0 +1,13 @@
+# Variables 
+
+my_variable = "My String variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
